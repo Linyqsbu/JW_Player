7f3e5d89c4999302ba38eb9e07357e3e0ed92c8d
YQLinsanity <yuqiang.lin@stonybrook.edu>
2021-10-19 10:14:07 -0400

    enabled auto play

--- JW_Player.js
+++ JW_Player.js
@@ -3,14 +3,27 @@ class JW_Player {
         this.width=width;
         this.height=height;
         this.container = document.getElementById(divId);
+        this.video=null;
+        this.autoplay=false;
     }
 
     load = (url) =>{
         let video = document.createElement("video");
-        video.style.width=this.width;
-        video.style.height=this.height;
-        addSourceToVideo(video, url);
+        video.width=this.width;
+        video.height=this.height;
+        video.src=url;
+        video.type="video/mp4";  
         this.container.append(video);
+        this.video=video;
+        if(this.autoplay){
+            if(this.video.muted){
+                this.video.muted = false;
+            } else {
+                debugger;
+                this.video.muted = true;
+                this.play()
+            }
+        }
     }
 
     getHeight = () => {
@@ -19,5 +32,33 @@ class JW_Player {
 
     getWidth = () => {
         return this.width;
+    }
+
+    play = () => {
+        this.video.play();
+    }
+
+    pause = () =>{
+        this.video.pause();
+    }
+
+    setAutoplay = (autoplay)=>{
+        this.autoplay=autoplay;
+    }
+
+    addButtons = () => {
+        let playButton = document.createElement("div");
+        playButton.classList.add("material-icons");
+        playButton.id="play-button";
+        playButton.innerHTML="play_arrow";  
+        this.container.append(playButton);
+        playButton.onmousedown=this.play;
+
+        let pauseButton = document.createElement("div");
+        pauseButton.classList.add("material-icons");
+        pauseButton.id="pause-button";
+        pauseButton.innerHTML="pause";
+        this.container.append(pauseButton);
+        pauseButton.onmousedown=this.pause;
     }
 }
